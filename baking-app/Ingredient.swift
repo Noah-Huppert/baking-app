bc6005fdcef3f715e882c8bcee885434709ece90
Noah Huppert <noahhuppert@gmail.com>
2016-01-19 13:41:57 -0500

    End of class

---
+++ baking-app/Ingredient.swift
@@ -0,0 +1,12 @@
+//
+//  Ingredient.swift
+//  baking-app
+//
+//  Created by block7 on 1/19/16.
+//  Copyright © 2016 noahhuppert.com. All rights reserved.
+//
+
+struct Ingredient {
+    var name: String
+    var ammount: Float
+}
---
+++ baking-app/Recipe.swift
@@ -0,0 +1,12 @@
+//
+//  Recipe.swift
+//  baking-app
+//
+//  Created by block7 on 1/19/16.
+//  Copyright © 2016 noahhuppert.com. All rights reserved.
+//
+
+struct Recipe {
+    var name: String
+    var ingredients: [Ingredient]
+}
