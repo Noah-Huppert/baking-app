2c13520855b3448371b806282a25498654667435
Noah Huppert <noahhuppert@gmail.com>
2016-02-03 12:14:57 -0500

    Created project

---
+++ baking-app/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  baking-app
+//
+//  Created by block7 on 2/3/16.
+//  Copyright (c) 2016 noahhuppert.com. All rights reserved.
+//
+
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+
+}
